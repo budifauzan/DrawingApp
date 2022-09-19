a67c7a008feced006c7c1dd6cc24f3d6fd2e1dc7
Budi Fauzan <budifauzan1997@gmail.com>
2022-09-19 20:18:18 +0700

    Add color selection

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,18 +4,31 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var ibBrushSize: ImageButton? = null
+    private var mIbCurrentColor: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val llPaintColors: LinearLayout = findViewById(R.id.ll_activity_main_paint_colors)
+        mIbCurrentColor = llPaintColors[2] as ImageButton
+        mIbCurrentColor!!.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.palette_selected
+            )
+        )
+
         drawingView = findViewById(R.id.dv_activity_main_canvas)
-        drawingView?.setSizeForBrush(20f)
+        drawingView?.setSizeForBrush(10f)
 
         ibBrushSize = findViewById(R.id.ib_activity_main_brush_size)
         ibBrushSize?.setOnClickListener {
