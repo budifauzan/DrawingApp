a86279212ca74945e8ef05f8c7449337b3cf9b20
Budi Fauzan <budifauzan1997@gmail.com>
2022-09-22 20:41:02 +0700

    Save image into file

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,20 +4,28 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,6 +43,8 @@ class MainActivity : AppCompatActivity() {
     private var ibColorWhite: ImageButton? = null
     private var ibGallery: ImageButton? = null
     private var ibUndo: ImageButton? = null
+    private var ibSave: ImageButton? = null
+    private var customProgressDialog: Dialog? = null
 
     val openGalleryLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -97,6 +107,7 @@ class MainActivity : AppCompatActivity() {
         ibColorWhite = findViewById(R.id.ib_activity_main_color_white)
         ibGallery = findViewById(R.id.ib_activity_main_pick_image)
         ibUndo = findViewById(R.id.ib_activity_main_undo)
+        ibSave = findViewById(R.id.ib_activity_main_save)
     }
 
     private fun setOnClick() {
@@ -130,8 +141,18 @@ class MainActivity : AppCompatActivity() {
         ibGallery?.setOnClickListener {
             requestStoragePermission()
         }
-        ibUndo?.setOnClickListener{
+        ibUndo?.setOnClickListener {
             drawingView?.undo()
+        }
+        ibSave?.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                showProgressDialog()
+                lifecycleScope.launch {
+                    val flContainer: FrameLayout = findViewById(R.id.fl_activity_main_container)
+                    saveBitmapFile(getBitmapFromView(flContainer))
+                }
+            }
+
         }
     }
 
@@ -187,6 +208,12 @@ class MainActivity : AppCompatActivity() {
         builder.create().show()
     }
 
+    private fun isReadStorageAllowed(): Boolean {
+        val result =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun requestStoragePermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
@@ -199,8 +226,85 @@ class MainActivity : AppCompatActivity() {
             )
 
         } else {
-            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
-            // TODO - add writing external storage permission
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val resultBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(resultBitmap)
+        val background = view.background
+        if (background != null) {
+            background.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+        return resultBitmap
+    }
+
+    private suspend fun saveBitmapFile(bitmap: Bitmap?): String {
+        var result = ""
+        withContext(Dispatchers.IO) {
+            if (bitmap != null) {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+
+                    val file = File(
+                        externalCacheDir?.absoluteFile.toString()
+                                + File.separator
+                                + "DrawingApp_"
+                                + System.currentTimeMillis() / 1000
+                                + ".png"
+                    )
+
+                    val fileOutput = FileOutputStream(file)
+                    fileOutput.write(bytes.toByteArray())
+                    fileOutput.close()
+
+                    result = file.absolutePath
+
+                    runOnUiThread {
+                        dismissProgressDialog()
+                        if (result.isNotEmpty()) {
+                            Snackbar.make(
+                                findViewById(R.id.ib_activity_main_save),
+                                "File saved successfully : $result",
+                                Snackbar.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Snackbar.make(
+                                findViewById(R.id.ib_activity_main_save),
+                                "Something went wrong while saving the file.",
+                                Snackbar.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
+    }
+
+    private fun showProgressDialog() {
+        customProgressDialog = Dialog(this)
+        customProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+        customProgressDialog?.show()
+    }
+
+    private fun dismissProgressDialog() {
+        if (customProgressDialog != null) {
+            customProgressDialog?.dismiss()
+            customProgressDialog = null
         }
     }
 }
