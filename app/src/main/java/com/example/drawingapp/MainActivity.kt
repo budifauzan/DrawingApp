a9bae53670155a70405e98460e7333a16eeeae8d
Budi Fauzan <budifauzan1997@gmail.com>
2022-09-22 20:49:51 +0700

    Done

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -278,6 +279,7 @@ class MainActivity : AppCompatActivity() {
                                 "File saved successfully : $result",
                                 Snackbar.LENGTH_SHORT
                             ).show()
+                            shareImage(result)
                         } else {
                             Snackbar.make(
                                 findViewById(R.id.ib_activity_main_save),
@@ -305,6 +307,16 @@ class MainActivity : AppCompatActivity() {
         if (customProgressDialog != null) {
             customProgressDialog?.dismiss()
             customProgressDialog = null
+        }
+    }
+
+    private fun shareImage(result: String) {
+        MediaScannerConnection.scanFile(this, arrayOf(result), null) { _, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
         }
     }
 }
